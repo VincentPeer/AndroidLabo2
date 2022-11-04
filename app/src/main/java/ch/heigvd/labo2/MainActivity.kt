f62fe49d32fa34ce7b3463000185d86ae3b127d7
VincentPeer <vincent.peer@heig-vd.ch>
2022-11-04 14:39:27 +0100

    ajout spinner et image cake

--- app/src/main/java/ch/heigvd/labo2/MainActivity.kt
+++ app/src/main/java/ch/heigvd/labo2/MainActivity.kt
@@ -2,10 +2,28 @@ package ch.heigvd.labo2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Nationalities management
+        val spinner = findViewById<Spinner>(R.id.spinner)
+
+        // https://developer.android.com/develop/ui/views/components/spinner
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.nationalities,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
     }
 }
