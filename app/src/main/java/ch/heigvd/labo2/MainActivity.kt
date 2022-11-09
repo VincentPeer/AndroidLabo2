92218fbb129c7ad5cb78155fd9608b9d8aa30e39
VincentPeer <vincent.peer@heig-vd.ch>
2022-11-09 22:07:05 +0100

    begin of formulaire validation

--- app/src/main/java/ch/heigvd/labo2/MainActivity.kt
+++ app/src/main/java/ch/heigvd/labo2/MainActivity.kt
@@ -1,39 +1,26 @@
 package ch.heigvd.labo2
 
 import android.os.Bundle
+import android.text.Editable
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.Group
 import ch.heigvd.labo2.Model.Person
+import ch.heigvd.labo2.Model.Student
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
-    //val people: List<Person> = listOf<Person>()
+    private var people = mutableListOf<Person>(Person.exampleStudent, Person.exampleWorker)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Nationalities management
-        val spinner = findViewById<Spinner>(R.id.nationality_spinner)
-
-        // https://developer.android.com/develop/ui/views/components/spinner
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter.createFromResource(
-            this,
-            R.array.nationalities,
-            android.R.layout.simple_spinner_item
-        ).also { adapter ->
-            // Specify the layout to use when the list of choices appears
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            // Apply the adapter to the spinner
-            spinner.adapter = adapter
-        }
-
-
         manageUserType()
+        //val radioGroup = findViewById<RadioGroup>(R.id.radio_group)
 
 
         val btnCancel = findViewById<Button>(R.id.btn_cancel)
@@ -49,40 +36,79 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun addNewPerson() {
-        val name: String = findViewById<EditText>(R.id.main_base_name).toString()
-        System.out.println("name : $name")
-    }
+        val name: String = findViewById<EditText>(R.id.main_base_name).text.toString()
+        val firstName: String = findViewById<EditText>(R.id.main_base_firstname).text.toString()
+        //val birthday: Calendar = findViewById<EditText>(R.id.main_base_firstname).text.toString()
+        val birthday: Calendar = Calendar.getInstance()
+        val nationality = getNationality()
+        val email: String = findViewById<EditText>(R.id.additional_mail).text.toString()
+        val remark: String = findViewById<EditText>(R.id.additional_remarks).text.toString()
+        if(manageUserType() == findViewById<RadioButton>(R.id.student_group).id) {
+            val university: String = findViewById<EditText>(R.id.main_specific_school).text.toString()
+            val graduationYear: Int = findViewById<EditText>(R.id.main_specific_graduationyear).text.toString().toInt()
+            val person = Student(name, firstName,birthday, nationality, university, graduationYear,
+            email, remark)
+            people.add(person)
 
-    private fun clearForm(group: ViewGroup) {
-        var i = 0
-        val count = group.childCount
-        while (i < count) {
-            val view = group.getChildAt(i)
-            if (view is EditText) {
-                view.setText("")
-            } else if(view is Spinner) {
-                view.setSelection(0) // Reset to "selection" state
-            }
-            ++i
         }
     }
 
-    /**
-     * Managing specific data whether the user is a student or a worker
-     */
-    private fun manageUserType() {
-        val studentGroup = findViewById<Group>(R.id.student_group)
-        val workerGroup = findViewById<Group>(R.id.worker_group)
-        val radioGroup = findViewById<RadioGroup>(R.id.radio_group)
-        radioGroup.setOnCheckedChangeListener { _, choiceId ->
-            if(choiceId == R.id.student_choice) {
-                studentGroup.visibility = View.VISIBLE
-                workerGroup.visibility = View.GONE
-            } else {
-                workerGroup.visibility = View.VISIBLE
-                studentGroup.visibility = View.GONE
+
+    // Nationalities management
+    private fun getNationality() : String {
+        val spinner = findViewById<Spinner>(R.id.nationality_spinner)
+        var nationality: String = ""
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                adapterView: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                nationality = spinner.getItemAtPosition(position).toString()
+            }
+
+            override fun onNothingSelected(adapterView: AdapterView<*>?) {
+                // Le Spinner ne permet pas de « désélectionner » une sélection
+                // ce callback est appelé uniquement dans le cas ou l’option sélectionnée est retirée du Spinner
             }
         }
+        return nationality
     }
 
-}
+        private fun clearForm(group: ViewGroup) {
+            var i = 0
+            val count = group.childCount
+            while (i < count) {
+                val view = group.getChildAt(i)
+                if (view is EditText) {
+                    view.setText("")
+                } else if(view is Spinner) {
+                    view.setSelection(0) // Reset to "selection" state
+                }
+                ++i
+            }
+        }
+
+        /**
+         * Managing specific data whether the user is a student or a worker
+         */
+        private fun manageUserType() : Int {
+            var id: Int = -1
+            val studentGroup = findViewById<Group>(R.id.student_group)
+            val workerGroup = findViewById<Group>(R.id.worker_group)
+            val radioGroup = findViewById<RadioGroup>(R.id.radio_group)
+            radioGroup.setOnCheckedChangeListener { _, choiceId ->
+                if(choiceId == R.id.student_choice) {
+                    studentGroup.visibility = View.VISIBLE
+                    workerGroup.visibility = View.GONE
+                } else {
+                    workerGroup.visibility = View.VISIBLE
+                    studentGroup.visibility = View.GONE
+                }
+                id = choiceId
+            }
+            return id
+        }
+
+    }
