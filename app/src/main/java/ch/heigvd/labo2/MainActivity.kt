c9d22e14edc65e6ef090edd2162b642943c143bc
VincentPeer <vincent.peer@heig-vd.ch>
2022-11-10 16:26:27 +0100

    added calendar

--- app/src/main/java/ch/heigvd/labo2/MainActivity.kt
+++ app/src/main/java/ch/heigvd/labo2/MainActivity.kt
@@ -1,7 +1,6 @@
 package ch.heigvd.labo2
 
 import android.os.Bundle
-import android.text.Editable
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
@@ -9,19 +8,43 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.Group
 import ch.heigvd.labo2.Model.Person
 import ch.heigvd.labo2.Model.Student
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.MaterialDatePicker
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
     private var people = mutableListOf<Person>(Person.exampleStudent, Person.exampleWorker)
+    private var personId: Int = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        manageUserType()
-        //val radioGroup = findViewById<RadioGroup>(R.id.radio_group)
+        val setBirthday = findViewById<ImageButton>(R.id.cake_button)
+        setBirthday.setOnClickListener {
 
+            val today = MaterialDatePicker.todayInUtcMilliseconds()
+            val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+
+            calendar.timeInMillis = today
+            calendar[Calendar.DAY_OF_MONTH] = 1
+            calendar[Calendar.MONTH] = Calendar.JANUARY
+            calendar[Calendar.YEAR] = 1996
+            val defaultDate = calendar.timeInMillis
+
+            val constraintsBuilder =
+                CalendarConstraints.Builder()
+                    .setOpenAt(defaultDate)
+            constraintsBuilder.build()
+            MaterialDatePicker.Builder.datePicker().setSelection(defaultDate)
+            .setCalendarConstraints(constraintsBuilder.build()).build().show(supportFragmentManager, "tag");
+        }
+
+        val radioGroup = findViewById<RadioGroup>(R.id.radio_group)
+        radioGroup.setOnCheckedChangeListener { _, choiceId ->
+            manageUserType(choiceId)
+        }
 
         val btnCancel = findViewById<Button>(R.id.btn_cancel)
         val parent = btnCancel.parent
@@ -39,17 +62,18 @@ class MainActivity : AppCompatActivity() {
         val name: String = findViewById<EditText>(R.id.main_base_name).text.toString()
         val firstName: String = findViewById<EditText>(R.id.main_base_firstname).text.toString()
         //val birthday: Calendar = findViewById<EditText>(R.id.main_base_firstname).text.toString()
-        val birthday: Calendar = Calendar.getInstance()
+        val birthday: Calendar = Calendar.getInstance().apply {
+            set(Calendar.YEAR, 1996)
+            set(Calendar.MONTH, Calendar.JUNE)
+            set(Calendar.DAY_OF_MONTH, 12) }
         val nationality = getNationality()
         val email: String = findViewById<EditText>(R.id.additional_mail).text.toString()
         val remark: String = findViewById<EditText>(R.id.additional_remarks).text.toString()
-        if(manageUserType() == findViewById<RadioButton>(R.id.student_group).id) {
+        if(personId == findViewById<RadioButton>(R.id.student_group).id) {
             val university: String = findViewById<EditText>(R.id.main_specific_school).text.toString()
             val graduationYear: Int = findViewById<EditText>(R.id.main_specific_graduationyear).text.toString().toInt()
-            val person = Student(name, firstName,birthday, nationality, university, graduationYear,
-            email, remark)
-            people.add(person)
-
+            val student =  Student(name, firstName,birthday, nationality, university, graduationYear, email, remark)
+            people.add(student)
         }
     }
 
@@ -76,39 +100,33 @@ class MainActivity : AppCompatActivity() {
         return nationality
     }
 
-        private fun clearForm(group: ViewGroup) {
-            var i = 0
-            val count = group.childCount
-            while (i < count) {
-                val view = group.getChildAt(i)
-                if (view is EditText) {
-                    view.setText("")
-                } else if(view is Spinner) {
-                    view.setSelection(0) // Reset to "selection" state
-                }
-                ++i
+    private fun clearForm(group: ViewGroup) {
+        var i = 0
+        val count = group.childCount
+        while (i < count) {
+            val view = group.getChildAt(i)
+            if (view is EditText) {
+                view.setText("")
+            } else if(view is Spinner) {
+                view.setSelection(0) // Reset to "selection" state
             }
+            ++i
         }
-
-        /**
-         * Managing specific data whether the user is a student or a worker
-         */
-        private fun manageUserType() : Int {
-            var id: Int = -1
-            val studentGroup = findViewById<Group>(R.id.student_group)
-            val workerGroup = findViewById<Group>(R.id.worker_group)
-            val radioGroup = findViewById<RadioGroup>(R.id.radio_group)
-            radioGroup.setOnCheckedChangeListener { _, choiceId ->
-                if(choiceId == R.id.student_choice) {
-                    studentGroup.visibility = View.VISIBLE
-                    workerGroup.visibility = View.GONE
-                } else {
-                    workerGroup.visibility = View.VISIBLE
-                    studentGroup.visibility = View.GONE
-                }
-                id = choiceId
-            }
-            return id
-        }
-
     }
+
+    /**
+     * Managing specific data whether the user is a student or a worker
+     */
+    private fun manageUserType(choiceId: Int) {
+        val studentGroup = findViewById<Group>(R.id.student_group)
+        val workerGroup = findViewById<Group>(R.id.worker_group)
+        if(choiceId == R.id.student_choice) {
+            studentGroup.visibility = View.VISIBLE
+            workerGroup.visibility = View.GONE
+        } else {
+            workerGroup.visibility = View.VISIBLE
+            studentGroup.visibility = View.GONE
+        }
+        this.personId = choiceId
+    }
+}
